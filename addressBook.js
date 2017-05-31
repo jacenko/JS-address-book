6b58c3510cb3a4846a2e4b105c985d65726e1b8b
Christopher Hernandez <christohernandez011@gmail.com>
2017-05-31 14:25:56 -0400

    Added More Contacts For testing.

--- addressBook.js
+++ addressBook.js
@@ -2,16 +2,39 @@ var contacts = {};
 contacts.jordan = {
   firstName: "Jordan",
   lastName: "Gates",
-  number: "(206) 555-5555",
-  address: ['One Microsoft Way','Redmond','WA','98052']
+  number: "(731) 474-1259",
+  address: ['3433 Melville Street','Memphis','TN','38118']
 };
 contacts.markus = {
-  firstName: "markus",
+  firstName: "Markus",
   lastName: "Knight",
-  number: "(408) 555-5555",
-  address: ['1 Infinite Loop','Cupertino','CA','95014']
+  number: "(573) 646-3300",
+  address: ['4375 John Daniel Drive','Bland','MO','65014']
 };
-
+contacts.thomas = {
+  firstName: "Thomas",
+  lastName: "Willett",
+  number: "(320) 525-2584",
+  address: ['4053 Brighton Circle Road','Sain Cloud','MN','56301']
+};
+contacts.jimmie = {
+  firstName: "Jimmie",
+  lastName: "Norman",
+  number: "(773) 209-7931",
+  address: ['3438 Oakmond Road', 'Arlington Heights', 'IL','60004']
+};
+contacts.bryon = {
+  firstName: "Bryon",
+  lastName: "Shafer",
+  number: "(626) 315-2920",
+  address: ['4945 Middleville Road', 'Sherman Oaks', 'CA','91403']
+};
+contacts.randolph = {
+  firstName: "Randolph",
+  lastName: "Hall",
+  number: "(858) 832-1616",
+  address: ['2679 Hood Avenue', 'El Cajon', 'CA','92020']
+};
 var list = function(obj) {
   for(var prop in obj) {
     console.log(prop);
@@ -27,5 +50,5 @@ var search = function(name) {
   }
 };
 
-// list(contacts);
-search("Jordan");
+list(contacts);
+search("Thomas");
