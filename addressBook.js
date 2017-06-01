bdd74bad765e0193ea87f7386ced8eab05f173b8
Deniss Jacenko <deniss.jacenko+github@gmail.com>
2017-05-31 22:53:10 -0400

    Detailed output and support for multiple results

--- addressBook.js
+++ addressBook.js
@@ -1,56 +1,97 @@
-var contacts = {};
-contacts.jordan = {
-  firstName: "Jordan",
-  lastName: "Gates",
-  number: "(731) 474-1259",
-  address: ['3433 Melville Street','Memphis','TN','38118']
-};
-contacts.markus = {
-  firstName: "Markus",
-  lastName: "Knight",
-  number: "(573) 646-3300",
-  address: ['4375 John Daniel Drive','Bland','MO','65014']
-};
-contacts.thomas = {
-  firstName: "Thomas",
-  lastName: "Willett",
-  number: "(320) 525-2584",
-  address: ['4053 Brighton Circle Road','Sain Cloud','MN','56301']
-};
-contacts.jimmie = {
-  firstName: "Jimmie",
-  lastName: "Norman",
-  number: "(773) 209-7931",
-  address: ['3438 Oakmond Road', 'Arlington Heights', 'IL','60004']
-};
-contacts.bryon = {
-  firstName: "Bryon",
-  lastName: "Shafer",
-  number: "(626) 315-2920",
-  address: ['4945 Middleville Road', 'Sherman Oaks', 'CA','91403']
-};
-contacts.randolph = {
-  firstName: "Randolph",
-  lastName: "Hall",
-  number: "(858) 832-1616",
-  address: ['2679 Hood Avenue', 'El Cajon', 'CA','92020']
-};
-var list = function(obj) {
-  for(var prop in obj) {
-    console.log(prop);
+var contacts = [
+  {
+    firstName: "Jordan",
+    lastName: "Gates",
+    number: "(731) 474-1259",
+    address: '3433 Melville Street',
+    city: 'Memphis',
+    state: 'TN',
+    zip: '38118'
+  },
+  {
+    firstName: "Markus",
+    lastName: "Knight",
+    number: "(573) 646-3300",
+    address: '4375 John Daniel Drive',
+    city: 'Bland',
+    state: 'MO',
+    zip: '65014'
+  },
+  {
+    firstName: "Thomas",
+    lastName: "Willett",
+    number: "(320) 525-2584",
+    address: '4053 Brighton Circle Road',
+    city: 'Sain Cloud',
+    state: 'MN',
+    zip: '56301'
+  },
+  {
+    firstName: "Jimmie",
+    lastName: "Norman",
+    number: "(773) 209-7931",
+    address: '3438 Oakmond Road',
+    city: 'Arlington Heights',
+    state: 'IL',
+    zip: '60004'
+  },
+  {
+    firstName: "Bryon",
+    lastName: "Shafer",
+    number: "(626) 315-2920",
+    address: '4945 Middleville Road',
+    city: 'Sherman Oaks',
+    state: 'CA',
+    zip: '91403'
+  },
+  {
+    firstName: "Randolph",
+    lastName: "Hall",
+    number: "(858) 832-1616",
+    address: '2679 Hood Avenue',
+    city: 'El Cajon',
+    state: 'CA',
+    zip: '92020'
   }
-};
+];
 
-var search = function(name) {
-  for(var prop in contacts) {
-    if(contacts[prop].firstName === name) {
-      console.log(contacts[prop]);
-      return contacts[prop];
-    } else {
-      alert("Sorry, contact not found. Please add to directory.");
+// Search contacts for a query string
+function search(query) {
+  console.log("Searching for \""+query+"\"...\n");
+  // Storage for found contacts
+  var result = [];
+  // Go through all contacts
+  for(var i = 0; i < contacts.length; i++) {
+    // Go through all keys in a contact
+    for(key in contacts[i]) {
+      // If key contains the query string
+      if(contacts[i][key].toLowerCase().indexOf(query.toLowerCase()) != -1) {
+        // Add the contact to found contacts
+        result.push(contacts[i]);
+      }
     }
   }
+  // Return array of found contacts
+  printResult(result);
 };
 
-// list(contacts);
+// Output results to console
+function printResult(result) {
+  if (result.length != 0) {
+    // Found at least one contact
+    console.log("%c I found something :)", "color: lime;");
+    for(var i = 0; i < result.length; i++) {
+      var c = result[i];
+      console.log("---\n"+c.firstName+" "+c.lastName);
+      console.log("\tfrom "+c.address+"\n\t"+c.city+", "+c.state+" "+c.zip+"\n");
+    }
+  } else {
+    // No contacts found
+    console.log("%c I didn't find anything :/\n", "color: coral;");
+  }
+  console.log("\n\n");
+}
+
+search("ran");
+search("g");
 search("chris");
